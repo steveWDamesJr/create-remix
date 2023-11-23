173bf1ef757d305de328a0637a128a007fc8ae0f
Steve <stevedamesjr@gmail.com>
2023-11-23 17:58:14 +0000

    Add edit form

--- my-remix-app/app/routes/contacts.$contactId_.edit.tsx
+++ my-remix-app/app/routes/contacts.$contactId_.edit.tsx
@@ -15,3 +15,62 @@ export const loader = async ({
     }
     return json({ contact });
 }
+
+export default function EditContact() {
+    const { contact } = useLoaderData<typeof loader>();
+
+    return(
+        <Form id="contact-form" method="post">
+            <p>
+                <span>Name</span>
+                <input
+                    defaultValue={contact.first}
+                    aria-label="First name"
+                    name="first"
+                    type="text"
+                    placeholder="First"
+                />
+                <input
+                    defaultValue={contact.last}
+                    aria-label="Last name"
+                    name="last"
+                    type="text"
+                    placeholder="Last"
+                />
+            </p>
+            <label>
+                <span>Twitter</span>
+                <input
+                    defaultValue={contact.twitter}
+                    name="twitter"
+                    type="text"
+                    placeholder="@jack"
+                >
+                </input>
+            </label>
+            <label>
+                <span>Avatar URL</span>
+                <input
+                    aria-label="Avatar URL"
+                    defaultValue={contact.avatar}
+                    name="avatar"
+                    type="text"
+                    placeholder="https://example.com/avatar.jpg"
+                >
+                </input>
+            </label>
+            <label>
+                <span>Notes</span>
+                <textarea
+                    defaultValue={contact.notes}
+                    name="notes"
+                    rows={6}
+                />
+            </label>
+            <p>
+                <button type="submit">Save</button>
+                <button type="button">Cancel</button>
+            </p>
+        </Form>
+    );
+}
