56236c7c4edcb8f3fb5bab28e7f1cfa5dd117d42
Steve <stevedamesjr@gmail.com>
2023-11-24 16:29:07 +0000

    Add optimistic UI on favorites

--- my-remix-app/app/routes/contacts.$contactId.tsx
+++ my-remix-app/app/routes/contacts.$contactId.tsx
@@ -103,7 +103,9 @@ const Favorite: FunctionComponent<{
     contact: Pick<ContactRecord, "favorite">;
 }> = ({ contact }) => {
     const fetcher = useFetcher();
-    const favorite = contact.favorite;
+    const favorite = fetcher.formData
+        ? fetcher.formData.get("favorite") === "true"
+        : contact.favorite;
 
     return (
         <fetcher.Form method="post">
